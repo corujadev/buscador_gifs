fabd23b66bfd5146d40c022b2f763f8a3e61b0af
Coruja Dev <vlemesalves@gmail.com>
2021-03-23 21:25:06 -0300

    Projeto Buscador de Gifs

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:buscador_gifs/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
